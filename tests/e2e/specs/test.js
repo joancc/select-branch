e86fdc19824d7c2a10eebdeef530d48962c3186f
Diana Carrillo <dianacarrillo.psic@outlook.com>
2019-01-21 13:47:40 -0600

    updating tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,49 +1,67 @@
-import { throwError } from "rxjs";
-import { cyan } from "ansi-colors";
-
-// https://docs.cypress.io/api/introduction/api.html
+// import { throwError } from "rxjs";
+// import { cyan } from "ansi-colors";
 
 describe("My First Test", () => {
-  // before each test
   beforeEach(() => {
     cy.visit("/");
   });
-  const user = cy;
-
-  it("Renders companies", () => {
-    user.get(".info").should("have.length.gt", 1);
+  it(".app element is visible", () => {
+    cy.get("#app").should("be.visible");
   });
-  it("highlights the selected company", () => {
-    user
-      .get("[data-testid='QA Gestionix_button']")
-      .click()
-      .should("have.class", "active");
-  });
-  it("Renders branches when a company is selected", () => {
-    user.get("[data-testid='QA Gestionix_button']").click();
-    user.get(".info").should("have.length.gt", 9);
-  });
-  it("Renders the hidden button", () => {
-    user
-      .get(".info")
-      .children()
-      .should("have.length", 2)
-      .get("button :nth-child(4)")
-      .click({ multiple: true })
-      .get("button .question");
-  });
-  // it("disabled unactive branches", () => {
-  //   user
-  //     .get("[data-testid='QA Gestionix_button']")
-  //     .click()
-  //     .get("[data-testid='QA Gestionix_button']")
-  //     .should("have.attr", "disabled");
-  // });
-  it("can authenticate", () => {
-    user.server().route({
+  it("Can authenticate", () => {
+    cy.server().route({
       method: "POST",
       url: "/ users / authentication",
       status: 200
     });
+  });
+  it("Can render companies", () => {
+    cy.server()
+      .route({
+        method: "GET",
+        url: "/users/companies"
+      })
+      .as("companiesColumn");
+  });
+
+  it("Renders the title for companies", () => {
+    cy.get("h4").contains("Empresas");
+  });
+
+  it("Renders companies", () => {
+    cy.get("div .info").should("have.length.gt", 1);
+  });
+  it("Highlights the selected company", () => {
+    cy.get("button")
+      .eq(0)
+      .click()
+      .should("have.class", "active");
+  });
+  it("Renders branches when a company is selected", () => {
+    cy.get("[data-testid='QA Gestionix_button']").click();
+    cy.get(".select-item").should("have.length.gt", 9);
+  });
+  it("Renders the hidden button", () => {
+    cy.get("button :nth-child(4)")
+      .click({ multiple: true })
+      .get("button .question");
+  });
+  it("Disables  unactive branches", () => {
+    cy.get("[data-testid='QA Gestionix_button']")
+      .click()
+      .get("[data-testid='4721_button']")
+      .should("have.attr", "disabled");
+  });
+
+  const getStore = () => cy.window().its("app.$store");
+  it("Has properties companies, branches, selectedCompanyId, selectedBranchId", () => {
+    getStore()
+      .its("state")
+      .should("have.keys", [
+        "companies",
+        "branches",
+        "selectedCompanyId",
+        "selectedBranchId"
+      ]);
   });
 });
